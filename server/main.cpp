b17ad7590c384a1351b1bcaf188791c5111925db
taviss <tavi7395@yahoo.com>
2017-10-18 20:05:40 +0300

    Blink.c reimplementation using custom classes -> main.cpp

--- server/main.cpp
+++ server/main.cpp
@@ -1,7 +1,27 @@
 #include <QApplication>
+#include <bcm2835.h>
+#include <outputpin.h>
 
 int main(int argc, char **argv)
 {
     QApplication app (argc, argv);
+    //Blink.c reimpl TODO - deleted this
+    if (!bcm2835_init())
+        return 1;
+
+    OutputPin pin(RPI_GPIO_P1_11);
+    while (1)
+    {
+        pin.set(HIGH);
+
+        // wait a bit
+        bcm2835_delay(500);
+
+        pin.set(LOW);
+
+        // wait a bit
+        bcm2835_delay(500);
+    }
+    bcm2835_close();
     return app.exec();
 }
