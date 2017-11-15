9677dfa328f13b6b9674da51e6043d588e4c9897
Tavi <tavi7395@yahoo.com>
2017-11-15 16:48:16 +0200

    Fixes

--- server/spi.cpp
+++ server/spi.cpp
@@ -26,19 +26,16 @@ int SPI::start()
         //printf("bcm2835_spi_begin failed. Are you running as root??\n");
         return -1;
     }
-    bcm2835_spi_setBitOrder(this->bitOrder);      // The default
-    bcm2835_spi_setDataMode(this->dataMode);                   // The default
-    bcm2835_spi_setClockDivider(this->clockDivider); // The default
-    bcm2835_spi_chipSelect(BCM2835_SPI_CS0);                      // The default
-    bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, this->csPolarity);      // the default
+    bcm2835_spi_setBitOrder(this->bitOrder);
+    bcm2835_spi_setDataMode(this->dataMode);
+    bcm2835_spi_setClockDivider(this->clockDivider);
+    bcm2835_spi_chipSelect(BCM2835_SPI_CS0);
+    bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, this->csPolarity);
     return 1;
 }
 
 uint8_t SPI::transfer(uint8_t data)
 {
     uint8_t read_data = bcm2835_spi_transfer(data);
-    //printf("Sent to SPI: 0x%02X. Read back from SPI: 0x%02X.\n", send_data, read_data);
-    //if (send_data != read_data)
-        //printf("Do you have the loopback from MOSI to MISO connected?\n");
     return read_data;
 }
